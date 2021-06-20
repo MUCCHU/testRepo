ac96da413e027f879c200e29e14b5139f6c9b65a
Sammy1836 <76647384+Sammy1836@users.noreply.github.com>
2021-06-21 00:53:41 +0530

    Add files via upload

---
+++ Empresario/js.js
@@ -0,0 +1,12 @@
+function emPdisplaYlogiNforM() {
+    document.getElementsByClassName("emP-bacKdroP-logiN")[0].style.display = "flex";
+}
+function emPhidElogiNforM() {
+    document.getElementsByClassName("emP-bacKdroP-logiN")[0].style.display = "none";
+}
+function emPdisplaYregistratioNforM() {
+    document.getElementsByClassName("emP-bacKdroP-registeR")[0].style.display = "flex";
+}
+function emPhidEregistratioNforM() {
+    document.getElementsByClassName("emP-bacKdroP-registeR")[0].style.display = "none";
+}
